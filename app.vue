2618ec6e5c9638fe8829007350a19cabb670c678
PeiYu <yupeiq1718@osensetech.com>
2023-07-21 17:59:03 +0800

    feat: add vr-video

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+import 'aframe'
+
+const vrVideoUrl = '/media/eagle-360.mp4'
+
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="m-2 relative w-full h-[600px]">
+    <a-scene vr-mode-ui="cardboardModeEnabled: true">
+      <a-assets>
+        <video id="video" :src="vrVideoUrl" loop="true" autoplay />
+      </a-assets>
+      <a-videosphere src="#video" rotation="0 180 0" />
+    </a-scene>
   </div>
 </template>
