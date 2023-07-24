5e75458146aaa3bff9f09ec2ac17bda81a5fbb55
PeiYu <yupeiq1718@osensetech.com>
2023-07-24 16:38:43 +0800

    feat: create video

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,30 @@ export default defineNuxtConfig({
       ]
     }],
     'nuxt-svgo'
-  ]
+  ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://vjs.zencdn.net/5.10.4/video-js.css'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.rawgit.com/yanwsh/videojs-panorama/master/dist/videojs-panorama.css'
+        }
+      ],
+      script: [
+        {
+          src: 'https://vjs.zencdn.net/5.10.4/video.js'
+        },
+        {
+          src: 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r76/three.js'
+        },
+        {
+          src: 'https://rawgit.com/yanwsh/videojs-panorama/master/dist/videojs-panorama.v5.js'
+        }
+      ]
+    }
+  }
 })
---
+++ pages/video.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+const options = {
+  plugins: {
+    panorama: {
+      clickAndDrag: true,
+      clickToToggle: true,
+      autoMobileOrientation: true,
+      backToVerticalCenter: false,
+      backToHorizonCenter: false
+    }
+  }
+}
+
+onMounted(() => {
+  window.videojs('player', options, () => {
+
+  })
+})
+
+</script>
+
+<template>
+  <div class="player-wrapper">
+    <video
+      id="player"
+      class="player video-js vjs-default-skin vjs-big-play-centered"
+      crossorigin="anonymous"
+      preload="metadata"
+      autoplay
+      controls
+    >
+      <source poster="https://raw.githubusercontent.com/Pierrinho/elephant/master/elephant.jpg" src="https://raw.githubusercontent.com/Pierrinho/elephant/master/elephant.mp4">
+    </video>
+  </div>
+</template>
+
+<style scoped>
+.player-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+
+  .player {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
