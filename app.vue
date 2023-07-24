634660c2c994b0d7724a58a1c4bcae5806689fd3
PeiYu <yupeiq1718@osensetech.com>
2023-07-24 11:37:04 +0800

    feat: move code from app to aframe

--- app.vue
+++ app.vue
@@ -1,17 +1,3 @@
-<script setup lang="ts">
-import 'aframe'
-
-const vrVideoUrl = '/media/eagle-360.mp4'
-
-</script>
-
 <template>
-  <div class="m-2 relative w-full h-[600px]">
-    <a-scene vr-mode-ui="cardboardModeEnabled: true">
-      <a-assets>
-        <video id="video" :src="vrVideoUrl" loop="true" autoplay />
-      </a-assets>
-      <a-videosphere src="#video" rotation="0 180 0" />
-    </a-scene>
-  </div>
+  <NuxtPage />
 </template>
---
+++ pages/aframe.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import 'aframe'
+
+const vrVideoUrl = '/media/eagle-360.mp4'
+
+</script>
+
+<template>
+  <div class="m-2 relative w-full h-[600px]">
+    <a-scene embedded class="w-[800px] h-[600px]">
+      <a-assets>
+        <video id="video" :src="vrVideoUrl" loop="true" autoplay crossorigin="anonymous" />
+      </a-assets>
+      <a-videosphere src="#video" rotation="0 180 0" />
+    </a-scene>
+  </div>
+</template>
